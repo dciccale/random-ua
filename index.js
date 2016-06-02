213429cb65d562c7aa977c7ab93ebd99c192f0b3
Denis Ciccale <dciccale@gmail.com>
2016-06-02 12:12:38 +0200

    use arrow fn and rename const

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const userAgents = require('./uas.json');
+const userAgentStrings = require('./uas.json');
 
-module.exports = function getUserAgent() {
-  return userAgents[Math.floor(Math.random() * userAgents.length)];
-};
+module.exports = () => userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)];
